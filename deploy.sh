9bbb9cc553a50641fb30b5c568c9252a1c22eb09
sipmann <sipmann@gmail.com>
2014-07-27 17:18:28 -0300

    Mais uma vez

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-BRANCH=master
-TARGET_REPO=pythonclub/pythonclub.github.io
+BRANCH=gh-pages
+TARGET_REPO=sipmann/sipmann.github.io
 PELICAN_OUTPUT_FOLDER=output
 
 echo -e "Testing travis-encrypt"
@@ -9,8 +9,8 @@ echo -e "$VARNAME"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
     if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "contato+pythonclub@xdvl.info"
-        git config --global user.name "PythonClub-Bot"
+        git config --global user.email "contato+sipmann@xdvl.info"
+        git config --global user.name "Sipmann-Bot"
     fi
     # using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
